611298a07a61219e35fc4080a412869bb6f7ed35
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-05-24 17:58:10 -0400

    Create Reservee.cs

---
+++ src/RobinApi.Net/Model/Reservee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace RobinApi.Net.Model
+{
+
+  [DataContract]
+  public class Reservee
+  {
+    
+    [DataMember(Name = "email", EmitDefaultValue = false)]
+    public string Email { get; set; }
+
+    [DataMember(Name = "user_id", EmitDefaultValue = false)]
+    public int UserId { get; set; }
+
+    [DataMember(Name = "visitor_id", EmitDefaultValue = false)]
+    public int? VisitorId { get; set; }
+
+    [DataMember(Name = "participation_status", EmitDefaultValue = false)]
+    public string ParticipationStatus { get; set; }
+  }
+
+}
