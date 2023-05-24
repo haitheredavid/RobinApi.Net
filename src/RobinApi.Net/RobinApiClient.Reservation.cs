1132507c590a30642f404f1c77b3abeb4ed12572
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-05-24 17:58:40 -0400

    Seat and Reservation API calls

---
+++ src/RobinApi.Net/RobinApiClient.Reservation.cs
@@ -0,0 +1,25 @@
+﻿using RobinApi.Net.Exceptions;
+using RobinApi.Net.Helpers;
+using RobinApi.Net.Model;
+using RobinApi.Net.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobinApi.Net
+{
+
+  public partial class RobinApiClient
+  {
+
+    public async Task<Reservation[]> GetReservations(KeyValuePair<string, string>[] query = null)
+    {
+      return await Get<Reservation[]>("reservations/seats", query);
+    }
+
+
+  }
+
+}
---
+++ src/RobinApi.Net/RobinApiClient.Seat.cs
@@ -0,0 +1,38 @@
+﻿using RobinApi.Net.Exceptions;
+using RobinApi.Net.Helpers;
+using RobinApi.Net.Model;
+using RobinApi.Net.Wrappers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobinApi.Net
+{
+
+  public partial class RobinApiClient
+  {
+    public async Task<Seat> GetSeat(int id)
+    {
+      var urlBuilder = new StringBuilder("seats/" + id);
+      var response = await _httpClient.GetAsync(urlBuilder.ToString()).ConfigureAwait(false);
+      var jsonResult = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+      if(response.IsSuccessStatusCode)
+      {
+        return JsonHelper.Deserialize<ApiWrapper<Seat>>(jsonResult).Data;
+      }
+      throw new RobinApiException(JsonHelper.Deserialize<ApiWrapper<object>>(jsonResult).Meta);
+    }
+
+    public async Task<Seat> GetSeatReservation(int id)
+    {
+      var urlBuilder = new StringBuilder("seats/" + id);
+      var response = await _httpClient.GetAsync(urlBuilder.ToString()).ConfigureAwait(false);
+      var jsonResult = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+      if(response.IsSuccessStatusCode)
+      {
+        return JsonHelper.Deserialize<ApiWrapper<Seat>>(jsonResult).Data;
+      }
+      throw new RobinApiException(JsonHelper.Deserialize<ApiWrapper<object>>(jsonResult).Meta);
+    }
+  }
+
+}
