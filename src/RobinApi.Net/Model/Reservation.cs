fc1999aeb91346b20d17b86fee20d4ea194c2b9d
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-05-24 17:58:15 -0400

    Create Reservation.cs

---
+++ src/RobinApi.Net/Model/Reservation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace RobinApi.Net.Model
+{
+
+  [DataContract]
+  public class Reservation
+  {
+    [DataMember(Name = "id", EmitDefaultValue = false)]
+    public string Id { get; set; }
+
+    [DataMember(Name = "group_seat_reservation_id", EmitDefaultValue = false)]
+    public int? GroupSeatReservationId { get; set; }
+
+    [DataMember(Name = "seat_id", EmitDefaultValue = false)]
+    public int SeatId { get; set; }
+
+    [DataMember(Name = "reserver_id", EmitDefaultValue = false)]
+    public string ReserverId { get; set; }
+
+    [DataMember(Name = "type", EmitDefaultValue = false)]
+    public string Type { get; set; }
+
+    [DataMember(Name = "title", EmitDefaultValue = false)]
+    public string Title { get; set; }
+
+    [DataMember(Name = "start", EmitDefaultValue = false)]
+    public DateTimeZone Start { get; set; }
+
+    [DataMember(Name = "end", EmitDefaultValue = false)]
+    public DateTimeZone End { get; set; }
+
+    [DataMember(Name = "recurrence", EmitDefaultValue = false)]
+    public string[] Recurrence { get; set; }
+
+    [DataMember(Name = "series_id", EmitDefaultValue = false)]
+    public string SeriesId { get; set; }
+
+    [DataMember(Name = "recurrence_id", EmitDefaultValue = false)]
+    public string RecurrenceId { get; set; }
+
+    [DataMember(Name = "created_at", EmitDefaultValue = false)]
+    public DateTime CreatedAt { get; set; }
+
+    [DataMember(Name = "updated_at", EmitDefaultValue = false)]
+    public DateTime UpdatedAt { get; set; }
+
+    [DataMember(Name = "reservee", EmitDefaultValue = false)]
+    public Reservee Reservee { get; set; }
+
+    [DataMember(Name = "status", EmitDefaultValue = false)]
+    public string Status { get; set; }
+  }
+
+}
